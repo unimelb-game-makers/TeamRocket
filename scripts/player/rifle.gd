34ade859be3e423ac3fe920dc1ec5fe8355d1584
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2024-11-22 14:44:35 +1100

    Added very basic ammo system

--- scripts/player/rifle.gd
+++ scripts/player/rifle.gd
@@ -17,14 +17,38 @@ var inaccuracy: float = 0.0:
 			inaccuracy_change_rate *= -1
 			inaccuracy_in = inaccuracy + inaccuracy_change_rate
 		inaccuracy = clamp(inaccuracy_in, -inaccuracy_limit, inaccuracy_limit)
-		
+
+@export var MAX_BULLETS = 5
+@export var FIRE_RATE = 1.0
+@export var RELOAD_SPEED = 0.6
+
+var bullets = MAX_BULLETS:
+	set(bullets_in):
+		bullets = bullets_in
+		if (bullets <= 0):
+			$ReloadTimer.start()
+
+@onready var fire_timer: Timer = $FireTimer
+@onready var reload_timer: Timer = $ReloadTimer
+
+func _ready() -> void:
+	fire_timer.wait_time = 1.0 / FIRE_RATE
+	reload_timer.wait_time = 1.0 / RELOAD_SPEED
+
 func _process(_delta):
 	look_at(get_global_mouse_position())
 	inaccuracy += inaccuracy_change_rate
 	raycast.rotation_degrees = inaccuracy
 	if Input.is_action_just_pressed("fire"):
-		print("Fired!")
-		print("Inaccuracy: " + str(inaccuracy))
-		var target_hit = raycast.get_collider()
-		if (target_hit):
-			print(target_hit)
+		if (fire_timer.is_stopped() and reload_timer.is_stopped()):
+			print("Inaccuracy: " + str(inaccuracy))
+			var target_hit = raycast.get_collider()
+			if (target_hit):
+				print(target_hit)
+			bullets -= 1
+			fire_timer.start()
+			print("Bullets: " + str(bullets))
+
+func _on_reload_timer_timeout() -> void:
+	print("Reloaded")
+	bullets = MAX_BULLETS
