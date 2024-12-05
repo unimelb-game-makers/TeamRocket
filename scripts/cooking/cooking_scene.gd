4765aae4c7d1f080420fe0519232b0392d9f029a
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2024-12-05 23:02:25 +1100

    Connected recipe output to cooking method

--- scripts/cooking/cooking_scene.gd
+++ scripts/cooking/cooking_scene.gd
@@ -2,7 +2,11 @@ class_name CookingScene extends Control
 
 @onready var activity_game: Control = %Activity
 @onready var ingredient_handler: HBoxContainer = %IngredientHandler
+@onready var start_button: TextureButton = $StartButton
+
 @export var activity_res: Activity
+
+var recipe: Recipe
 
 signal complete(output)
 
@@ -11,15 +15,19 @@ func _ready() -> void:
 	ingredient_handler.max_slots = activity_res.max_ingredients
 	ingredient_handler.create_slots()
 	Globals.inventory_ui.toggle_inventory(true)
-
-func start() -> void:
-	activity_game.start()
-	Globals.inventory_ui.toggle_inventory(false)
 	
 func add_item(item: Item):
 	ingredient_handler.add_item(item)
 
 func finish():
-	print("Chop Signal Received")
 	complete.emit()
-	
+	Inventory_Global.add_item(recipe.output_item, 1)
+
+func _on_start_button_pressed() -> void:
+	recipe = activity_res.match_recipe(ingredient_handler.selected_ingredients)
+	if (recipe):
+		start_button.visible = false
+		ingredient_handler.visible = false
+		activity_game.visible = true
+		activity_game.start()
+		Globals.inventory_ui.toggle_inventory(false)
