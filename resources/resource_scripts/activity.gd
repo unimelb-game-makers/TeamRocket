1d75af9433f894af01fe0048cff6f1425ea71368
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2024-12-21 21:46:09 +1100

    Added bolognaise recipe

--- resources/resource_scripts/activity.gd
+++ resources/resource_scripts/activity.gd
@@ -8,9 +8,7 @@ func match_recipe(items: Array[Item]) -> Recipe:
 	for recipe in recipes:
 		var matched_items = []
 		var temp_items = items.duplicate(true)
-		print(temp_items)
 		for ingredient in recipe.required_items:
-			print(ingredient)
 			if temp_items.has(ingredient):
 				matched_items.append(ingredient)
 				temp_items.erase(ingredient)
