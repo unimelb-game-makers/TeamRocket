c4f03c3b71f4fefc3ff7fda41f90c32bd4270c5e
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2024-12-23 18:34:10 +1100

    Added better ui for gun

--- scripts/player/rifle.gd
+++ scripts/player/rifle.gd
@@ -9,14 +9,7 @@ extends Node2D
 var inaccuracy_limit: float = 0.0:
 	set(limit):
 		inaccuracy_limit = clamp(limit, 0, MAX_INACCURACY)
-
 var inaccuracy_change_rate: float = INACCURACY_CHANGE_RATE_BASE
-var inaccuracy: float = 0.0: 
-	set(inaccuracy_in):
-		if (inaccuracy >= inaccuracy_limit or inaccuracy <= -inaccuracy_limit):
-			inaccuracy_change_rate *= -1
-			inaccuracy_in = inaccuracy + inaccuracy_change_rate
-		inaccuracy = clamp(inaccuracy_in, -inaccuracy_limit, inaccuracy_limit)
 
 @export var MAX_BULLETS = 5
 @export var FIRE_RATE = 1.0
@@ -24,6 +17,7 @@ var inaccuracy: float = 0.0:
 
 @onready var aiming_line_1 = $AimingUI/Line1
 @onready var aiming_line_2 = $AimingUI/Line2
+@onready var aiming_curve = $AimingUI
 
 var gun_enabled = true
 
@@ -43,13 +37,12 @@ func _ready() -> void:
 func _process(_delta):
 	if (gun_enabled):
 		look_at(get_global_mouse_position())
-		inaccuracy += inaccuracy_change_rate
-		aiming_line_1.rotation_degrees = -inaccuracy
-		aiming_line_2.rotation_degrees = inaccuracy
-		raycast.rotation_degrees = inaccuracy
+		aiming_line_1.rotation_degrees = -inaccuracy_limit
+		aiming_line_2.rotation_degrees = inaccuracy_limit
+		update_aiming_ui()
 		if Input.is_action_just_pressed("fire"):
 			if (fire_timer.is_stopped() and reload_timer.is_stopped()):
-				print("Inaccuracy: " + str(inaccuracy))
+				raycast.rotation_degrees = randf_range(-inaccuracy_limit, inaccuracy_limit)
 				var target_hit = raycast.get_collider()
 				if (target_hit):
 					print(target_hit)
@@ -58,5 +51,8 @@ func _process(_delta):
 				print("Bullets: " + str(bullets))
 
 func _on_reload_timer_timeout() -> void:
-	print("Reloaded")
 	bullets = MAX_BULLETS
+
+func update_aiming_ui() -> void:
+	aiming_curve.angle = inaccuracy_limit
+	aiming_curve.queue_redraw()
