654ce61d83196103ba67ed365e5b380add853c44
Lok/Tom Chow <lohchness@gmail.com>
2025-02-03 17:12:25 +1100

    Expose radius of collisionshape to scene root

--- scripts/enemy/Areas/spawnarea.gd
+++ scripts/enemy/Areas/spawnarea.gd
@@ -5,30 +5,38 @@ extends Area2D
 # Make sure this is not 0
 @export var allowed_enemies: Array[PackedScene] = []
 
-@onready var radius = $CollisionShape2D.shape.radius
+# Expose CollisionShape radius to scene root in editor
+@onready var collisionshape: CollisionShape2D = $CollisionShape2D
+@export var radius: float:
+	set(x):
+		radius = x
+		if collisionshape:
+			collisionshape.radius = x
+
 
 func _ready() -> void:
-	assert(len(allowed_enemies) > 0) 
-
-func spawn_single():
+	#assert(len(allowed_enemies) > 0)
+	#assert(allowed_enemies.size() > 0)
+	print(allowed_enemies.size())
+	print(radius)
 	
+	collisionshape.set("radius", radius)
+
+# Make sure 
+func spawn_single():
 	var e = allowed_enemies.pick_random().instantiate()
-	e.global_position = global_position + radius * get_random_point()
+	e.global_position = global_position + collisionshape.radius * get_random_point()
 	
 	# Add child to root node
 	get_tree().root.add_child(e)
 
 
 func spawn_batch(amount: int):
-	
-	pass
+	for i in range(amount):
+		spawn_single()
 
 func get_random_point():
-	var x = sin(randf_range(-radius, radius))
-	var y = cos(randf_range(-radius, radius))
+	var r = collisionshape.radius
+	var x = sin(randf_range(-r, r))
+	var y = cos(randf_range(-r, r))
 	return Vector2(x , y)
-
-# Testing purposes
-func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("roll"):
-		spawn_single()
