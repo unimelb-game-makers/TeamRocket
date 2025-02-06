f12cc31e0f11ec721b72f033869d76f28fcf5cd4
Lok/Tom Chow <lohchness@gmail.com>
2025-02-06 17:42:16 +1100

    Dash attack 3 times. Damage not implemented yet

--- scripts/enemy/enemy_dasher.gd
+++ scripts/enemy/enemy_dasher.gd
@@ -1,8 +1,7 @@
 extends Basic_Enemy
 
-
 var dash_attack_vector: Vector2 # Position of player when winding up
-var dash_attack_speed: float = 1500
+var dash_attack_speed: float = 1000
 
 var dash_attack_timer: float = 0
 var dash_attack_duration: float = 1
@@ -10,37 +9,58 @@ var dash_attack_duration: float = 1
 var current_dashes = 0
 var num_dashes = 3
 
+var in_attack_state: bool = false
+
+# Stop moving, and prepare to dash. Delay to Dash Attack set in Inspector
 func _on_wind_up_state_entered() -> void:
 	velocity = Vector2.ZERO
-	dash_attack_vector = Vector2(target_creature.position)
-
-
+	
 func _on_wind_up_state_physics_processing(delta: float) -> void:
 	move_and_slide()
 
+# Get direction to player
 func _on_dash_attack_state_entered() -> void:
+	dash_attack_timer = 0
 	current_dashes += 1
-	
+	dash_attack_vector = global_position.direction_to(target_creature.global_position)
 
+# Dash to player. Dash speed determined by dash_attack_duration.
 func _on_dash_attack_state_physics_processing(delta: float) -> void:
 	dash_attack_timer += delta
 	if dash_attack_timer >= dash_attack_duration:
+		print(dash_attack_timer)
 		$StateChart.send_event("on_dash_attack_finish")
 	else:
 		velocity = dash_attack_vector * roll_speed(dash_attack_timer)
 		move_and_slide()
 
+# Exit Attack state after dashing 3 times.
 func _on_dash_attack_state_exited() -> void:
 	# Exit entire Attack state, go back to Active state
-	if current_dashes == num_dashes:
+	if current_dashes >= num_dashes:
 		statechart.send_event("on_attack_finish")
 
 
 func roll_speed(elapsed_time : float) -> float:
-	var roll_start_speed_multiplier = 1.5
-	var roll_end_speed_multiplier = 0.5
-	var t = elapsed_time / dash_attack_duration
-	var curve = 1 - t * t  # Quick start and smooth slowdown
-	var start_speed = dash_attack_speed * roll_start_speed_multiplier
-	var end_speed = PASSIVE_SPEED * roll_end_speed_multiplier
-	return start_speed - (start_speed - end_speed) * curve
+	var t : float = elapsed_time / dash_attack_duration
+	return dash_attack_speed - (dash_attack_speed - PASSIVE_SPEED) * t * t
+
+# Overrides function in enemy_basic. Checks if this enemy is in the Attack state.
+func _on_chase_radius_area_exited(area: Area2D) -> void:
+	if area.is_in_group("Player") and not in_attack_state:
+		
+		# When chasing, take note of last known position. 
+		# This enemy will travel to this position to search,
+		# only after path has no more points.
+		last_known_position = target_creature.position
+		
+		target_creature = null
+		statechart.send_event("target_exit_chase_radius") # Triggers To Search
+
+func _on_attack_state_entered() -> void:
+	in_attack_state = true
+
+func _on_attack_state_exited() -> void:
+	current_dashes = 0
+	dash_attack_timer = 0
+	in_attack_state = false
