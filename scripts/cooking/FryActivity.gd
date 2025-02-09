fd5be1e90f07f1c3c556348a054cfe4c1e131b44
X13warzone <bobthenim@yahoo.com.au>
2025-02-09 12:59:40 +1100

    Minor update to frying
    - Added temporary animation for flipping and dropping ingredients
    - Fixed a bug with ingredient being able to move outside of pan
    Todo:
    - Add in UI elements (some of the template is already present, only need assets)

--- scripts/cooking/FryActivity.gd
+++ scripts/cooking/FryActivity.gd
@@ -18,7 +18,8 @@ signal complete(output)
 @onready var progress_debug: Label = $ProgressDebug
 
 # Frying pan size
-var pan_size_squared := 1600
+const PAN_SIZE = 60
+const PAN_SIZE_SQUARED = 3600
 
 # How long to fry for
 var progress: Array = [0, 0]
@@ -38,12 +39,19 @@ var velocity: Vector2
 var fried = 0
 @export var target = 2
 
+# This will go to false when flipping the pan
+var cooking = false
+
+# This will go to false when the activity is finished
 var playing = false
 
 
 func start() -> void:
-	ingr.texture # = ingredient item texture here
+	#ingr.texture # = ingredient item texture here
 	playing = true
+	cooking = true
+	drop_ingredient()
+	
 
 
 func _process(delta: float) -> void:
@@ -53,8 +61,8 @@ func _process(delta: float) -> void:
 		# Note on using progess[side - 1]
 		# This actually always works; if side == 0 -> progress[-1] -> progress[1]
 		# if side == 1 -> progress[0]
-		progress[side - 1] += delta * calculate_area()
-		ingr.self_modulate.v = (target_progress - progress[side] * 0.75) / (target_progress)
+		if cooking:
+			progress[side - 1] += delta * calculate_area()
 		if progress[0] >= target_progress and progress[1] >= target_progress:
 			print("Fried successfully!")
 			fried += 1
@@ -64,6 +72,7 @@ func _process(delta: float) -> void:
 			progress.clear()
 			progress.append(0)
 			progress.append(0)
+			drop_ingredient()
 		if progress[side - 1] >= target_progress:
 			if warning_timer <= 0:
 				warning_timer = 0.5
@@ -72,40 +81,58 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	if playing:
+	if playing and cooking:
 		var dir = Input.get_vector("left", "right", "up", "down")
 		if dir:
 			velocity += dir.normalized()
 		else:
-			velocity.move_toward(Vector2.ZERO, delta)
+			velocity.move_toward(Vector2.ZERO, delta * PAN_SIZE)
 		if sizzle_timer <= 0:
+			# This moves the ingredient randomly
 			sizzle_timer = randf_range(1, 4)
+			velocity *= randf_range(0.7, 1.2)
 			velocity += Vector2(randf_range(0, 5), randf_range(0, 5))
-		move_ingredient(delta)
+	move_ingredient(delta)
 
  
 func move_ingredient(delta: float) -> void:
 	ingr.position += velocity * delta
-	ingr.position.limit_length(40)
-	#ingr.position = ingr.position.clamp(Vector2(-pan_size.x, -pan_size.y), Vector2(pan_size.x, pan_size.y))
+	if ingr.position.length_squared() >= PAN_SIZE_SQUARED:
+		velocity *= 0.5
+		ingr.position = ingr.position.limit_length(PAN_SIZE)
 
 
 func calculate_area() -> float:
 	var d = ingr.position.length_squared()
-	if d < pan_size_squared / 4.0:
+	if d < PAN_SIZE_SQUARED / 4.0:
 		return 1
-	if d < pan_size_squared / 2.0:
+	if d < PAN_SIZE_SQUARED / 2.0:
 		return 0.75
-	if d < 3 * pan_size_squared / 4.0:
+	if d < 3 * PAN_SIZE_SQUARED / 4.0:
 		return 0.25
 	return 0.1
 
 
+# Currently, this is only for flipping ingredient
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("roll"):
+	if event.is_action_pressed("roll") and cooking:
+		cooking = false
 		side = (side + 1) % 2
 		warning.hide()
 		warning_timer = 0
+		ingr.self_modulate.v = (target_progress - progress[side] * 0.6) / (target_progress)
+		$AnimationPlayer.play("flip")
+		await $AnimationPlayer.animation_finished
+		cooking = true
+
+
+func drop_ingredient() -> void:
+	cooking = false
+	ingr.position = Vector2.ZERO
+	ingr.self_modulate.v = (target_progress - progress[side] * 0.6) / (target_progress)
+	$AnimationPlayer.play("drop")
+	await $AnimationPlayer.animation_finished
+	cooking = true
 
 
 func finish() -> void:
