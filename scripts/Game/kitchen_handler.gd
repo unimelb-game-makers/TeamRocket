6300237b72ef375aaf6150317d11ffbccf22c950
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2025-02-10 17:20:42 +0800

    Added altar

--- scripts/Game/kitchen_handler.gd
+++ scripts/Game/kitchen_handler.gd
@@ -1,8 +1,12 @@
 extends Node
 
+var sacrificed = false
+
 func _on_exit_area_body_entered(body: Node2D) -> void:
 	switch_to_city()
 	
 func switch_to_city():
-	print("Switching")
 	get_tree().change_scene_to_file("res://scenes/environments/City.tscn")
+
+func sacrifice():
+	pass
