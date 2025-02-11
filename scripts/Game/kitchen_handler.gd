c3518463616e580e741d0b57859655b0e6a5b943
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2025-02-11 12:12:14 +0800

    Temp buffs

--- scripts/Game/kitchen_handler.gd
+++ scripts/Game/kitchen_handler.gd
@@ -11,4 +11,10 @@ func switch_to_city():
 func submit_food(item: Item):
 	var buff_i = randi_range(0,3)
 	print(buff_i)
-	
+	match buff_i:
+		1:
+			Globals.player_hp_increase += 0.2
+		2:
+			Globals.player_speed_increase += 0.2
+		3:
+			Globals.player_damage_increase += 0.2
