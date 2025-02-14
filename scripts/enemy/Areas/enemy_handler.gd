485b446618fb4220b78f067366a406dfe88a23b2
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2025-02-15 01:10:33 +0800

    Adjusted mob spawns

--- scripts/enemy/Areas/enemy_handler.gd
+++ scripts/enemy/Areas/enemy_handler.gd
@@ -1,8 +1,12 @@
 extends Node
 
-@export var spawns_per_area: int = 1
+@export var min_spawns_per_area: int = 1
+@export var max_spawns_per_area: int = 1
 @onready var spawn_areas: Node = $SpawnAreas
 
 func _ready() -> void:
 	for area in spawn_areas.get_children():
-		area.spawn_batch(spawns_per_area)
+		var spawn_mob = randf() < 0.5
+		if spawn_mob:
+			var spawns = randi_range(min_spawns_per_area, max_spawns_per_area)
+			area.spawn_batch(spawns)
