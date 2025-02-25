339cfb48f8f5ed0e599728f5a40186a8d2244a15
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2025-02-26 00:47:48 +1100

    Fixed some kitchen stuff reverting to old versions

--- scripts/ui/ui.gd
+++ scripts/ui/ui.gd
@@ -2,11 +2,7 @@ extends Control
 
 @onready var inventory: Control = $CanvasLayer/Inventory
 
-@onready var cooking_handler: Control = $CanvasLayer/CookingHandler
 var inventory_container: Control
 
 func _ready() -> void:
 	inventory_container = inventory.inventory_container
-
-func _on_player_activity_interact(activity: Variant) -> void:
-	cooking_handler.init_activity(activity)
