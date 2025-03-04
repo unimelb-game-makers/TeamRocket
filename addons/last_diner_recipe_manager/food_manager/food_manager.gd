7ca3fab73b6aec21b43a27cdd647d1cd0e446aa8
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2025-03-04 15:26:06 +1100

    Added basic save/edit functionality

--- addons/last_diner_recipe_manager/food_manager/food_manager.gd
+++ addons/last_diner_recipe_manager/food_manager/food_manager.gd
@@ -11,6 +11,9 @@ extends Control
 @onready var save_button: Button = $FoodEditorUI/SaveButton
 @onready var delete_button: Button = $FoodEditorUI/DeleteButton
 
+@export var food_res_folder: String
+
+var editing_item: Item
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,4 +23,21 @@ func update_food_editor(item: Item, file: String) -> void:
 	food_name_input.text = item.item_name
 	file_name_input.text = file
 	weight_input.text = str(item.weight)
+	texture_button.texture_normal = item.texture
 	description_input.text = item.description
+	editing_item = item.duplicate()
+
+func _on_texture_button_pressed() -> void:
+	print("Want to change texture")
+
+func _on_save_button_pressed() -> void:
+	print("Saving Resource")
+	editing_item.item_name = food_name_input.text
+	editing_item.weight = float(weight_input.text)
+	editing_item.texture = texture_button.texture_normal
+	editing_item.description = description_input.text
+	ResourceSaver.save(editing_item, food_res_folder + file_name_input.text)
+
+func _on_delete_button_pressed() -> void:
+	print("Are you sure you want to do that?")
+	pass
