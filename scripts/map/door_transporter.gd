4064fe328cf800508704722c52390dbf1595996c
Lok/Tom Chow <lohchness@gmail.com>
2025-03-15 18:47:33 +1100

    Change scene on room enter area

--- scripts/map/door_transporter.gd
+++ scripts/map/door_transporter.gd
@@ -5,4 +5,7 @@ class_name DoorArea
 @export_range(0, 3, 1) var direction = 0
 
 # Which room this door leads into (set by script)
-var target_room
+var target_room: PackedScene
+
+func _on_area_entered(area: Area2D) -> void:
+	get_tree().change_scene_to_packed(target_room)
