dee7905d8216cff0a9fb5725d155d704442a63de
NKLam <khoalamvn@gmail.com>
2025-03-20 00:40:17 +1100

    Add 2d outline for kitchen interactable

--- scripts/cooking/activity_station.gd
+++ scripts/cooking/activity_station.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
 @export var activity: PackedScene
+
+@onready var sprite: Sprite2D = $Sprite2D
 @onready var cooking_scene: CookingScene = $CanvasLayer/CookingScene
+
+
+func _ready() -> void:
+	sprite.material.set_shader_parameter("outline_color", Color.YELLOW)
 
 func reset_cooking():
 	cooking_scene.reset()
@@ -13,9 +19,14 @@ func interact():
 		reset_cooking()
 		cooking_scene.show()
 
-func _on_cooking_scene_complete(output: Variant) -> void:
+func _on_cooking_scene_complete(_output: Variant) -> void:
 	cooking_scene.hide()
 
-func _on_body_exited(body: Node2D) -> void:
+func _on_body_exited(_body: Node2D) -> void:
 	reset_cooking()
 	cooking_scene.hide()
+	sprite.material.set_shader_parameter("outline_color", Color.YELLOW)
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	sprite.material.set_shader_parameter("outline_color", Color.GREEN)
