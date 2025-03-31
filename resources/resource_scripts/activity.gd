7fdba8b7338d3d4c4e098883f69d1e80cfa09588
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2025-03-31 18:42:54 +1100

    Added chopped chicken ingredient

--- resources/resource_scripts/activity.gd
+++ resources/resource_scripts/activity.gd
@@ -5,10 +5,7 @@ extends Resource
 @export var max_ingredients: int
 
 func match_recipe(items: Array[Ingredient]) -> Recipe:
-	print("Matching Recipe")
 	for recipe in recipes:
-		print(recipe)
 		if recipe.generate_item(items):
-			print("Recipe Found")
 			return recipe
 	return null
