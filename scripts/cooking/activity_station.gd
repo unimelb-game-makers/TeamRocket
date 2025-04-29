7a8765a63b1b852f5823b1a8f14b5988cf2972db
X13warzone <bobthenim@yahoo.com.au>
2025-04-29 18:19:30 +1000

    Fixed "ESC" not closing out of workstation menus
    Fixed bug #67

--- scripts/cooking/activity_station.gd
+++ scripts/cooking/activity_station.gd
@@ -13,10 +13,19 @@ func interact():
 	if cooking_scene.activity_is_in_progress:
 		return
 	if (cooking_scene.visible):
-		cooking_scene.hide()
+		exit_cooking()
 	else:
 		reset_cooking()
 		cooking_scene.show()
+
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause") and (cooking_scene.visible):
+		exit_cooking()
+		get_viewport().set_input_as_handled()
+
+func exit_cooking() -> void:
+	cooking_scene.hide()
 
 func _on_cooking_scene_complete(_output: Variant) -> void:
 	cooking_scene.hide()
