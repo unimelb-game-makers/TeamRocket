bdec5fab0803c2f4a32779aca0806db805239b14
junyynyyn <7540864+junyynyyn@users.noreply.github.com>
2025-05-04 22:03:52 +1000

    Removed Version 2 Dish Crafting System from files

--- CookingSystemVersion3/testing/food_test.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-@export var workbench: Array[Ingredient]
-@export var crafting_station: CraftingStation
-
-func _ready() -> void:
-	var output = crafting_station.craft_output(workbench)
-	print(output.save())
-	print(Item.load(output.save()).save())
--- DishCreationFramework2/dcf_ingredient_recipe.gd
+++
@@ -1,5 +0,0 @@
-class_name DCF_IngredientRecipe
-extends Resource
-
-@export var input_ingredients: Array[DCF_Ingredient.IngredientType]
-@export var output_ingredient: DCF_Ingredient
