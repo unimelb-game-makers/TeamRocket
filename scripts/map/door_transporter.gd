9283cd75344ba7ad5fa214f745af1abb9c2bb4dd
Lok/Tom Chow <lohchness@gmail.com>
2025-05-05 18:21:35 +1000

    Door area entered to body entered

--- scripts/map/door_transporter.gd
+++ scripts/map/door_transporter.gd
@@ -10,10 +10,11 @@ var door_direction: Vector2
 # True if player enters the room through this door. Blocks _on_area_entered.
 var is_entered: bool = false 
 
-func _on_area_entered(area: Area2D) -> void:
-	if not is_entered:
-		go_to_room.emit(door_direction)
-
 # If player spawns on this door, is_entered will be true.
 func _on_area_exited(area: Area2D) -> void:
 	is_entered = false
+
+func _on_body_entered(body: Node2D) -> void:
+	if not is_entered:
+		print("Direction: " + str(door_direction))
+		go_to_room.emit(door_direction)
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -146,7 +146,7 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("reload"):
 		rifle.reload()
 		
-	global_rotation = 0
+	#global_rotation = 0
 
 func damage(value: int):
 	if is_invulnerable_after_hurt:
