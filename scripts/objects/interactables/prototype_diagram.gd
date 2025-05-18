cb83c4cfa9920e03888af8283a82f06c32b5b711
NKLam <khoalamvn@gmail.com>
2025-05-19 04:22:17 +1000

    Update recipe book

--- scripts/objects/interactables/prototype_diagram.gd
+++ scripts/objects/interactables/prototype_diagram.gd
@@ -4,9 +4,15 @@ extends Area2D
 @onready var sprite: Sprite2D = $PrototypeDiagram
 
 func _ready():
+	recipe_ui.visible = false
 	sprite.material.set_shader_parameter("outline_color", Color.YELLOW)
 
 func interact():
+	if recipe_ui.visible:
+		Globals.player.set_player_movement(true)
+	else:
+		Globals.player.set_player_movement(false)
+
 	recipe_ui.visible = not recipe_ui.visible
 
 func _on_area_exited(_area: Area2D) -> void:
