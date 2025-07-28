634ecfada34d8c177294a1b8dde57eb35e2239f1
GcNoteX <gcha0054@student.monash.edu>
2025-07-28 21:13:31 +1000

    Implemented levels and level randomization for BakeActivity
    - Created new Oven Tracing Line Data for the activity.
    - Created new MinigameSettings class which holds the difficulty of the associated settings.
    - Oven Activity determines which map to choose based on the probability of the inverse of each difficulty setting.

--- scripts/cooking/cooking_activity.gd
+++ scripts/cooking/cooking_activity.gd
@@ -16,13 +16,19 @@ func initialize_activity(inputs: Array[Ingredient], output: Item):
 	if output == null:
 		push_error("Initializing activity with no output ingredient")
 		return
-		
+
 	self.input_ingredients = inputs
 	self.output_item = output
 	is_initialized = true
 
 func minigame_complete() -> void:
+	push_warning("Minigame completed function not implemented in child!!!")
 	pass # Implement in child
 
 func finish(rating: Item.Quality):
 	complete.emit(rating)
+
+#static func create_activity(settings: MinigameSettings) -> CookingActivity:
+	#push_error("Needs to be implemented in child!!!")
+	#var ins: CookingActivity = load("res://scenes/cooking/activities/ChoppingActivity.tscn").instantiate()
+	#return ins
