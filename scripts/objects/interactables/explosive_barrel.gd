8d953ef6b3526451dad4e6681bf3e5aef532d64a
NKLam <khoalamvn@gmail.com>
2025-07-29 22:48:07 +1000

    Add explosive barrel and fire

---
+++ scripts/objects/interactables/explosive_barrel.gd
@@ -0,0 +1,27 @@
+extends StaticBody2D
+
+@export var fire_scene: PackedScene
+@export var explosion_damage: int = 100
+
+@onready var explosion_range: Area2D = $ExplosionRange
+
+func damage(_value: int):
+	var fire_inst = fire_scene.instantiate()
+	Globals.interactable_handler.interactable_holder.add_child(fire_inst)
+	fire_inst.global_position = global_position
+	# TODO: Play explosive VFX and SFX here
+	explosion()
+	queue_free()
+
+func _on_environment_hitbox_damaged(value: int) -> void:
+	damage(value)
+
+
+func explosion():
+	for area in explosion_range.get_overlapping_areas():
+		if area.has_method("damage"):
+			area.damage(explosion_damage)
+
+	for body in explosion_range.get_overlapping_bodies():
+		if body.has_method("damage"):
+			body.damage(explosion_damage)
