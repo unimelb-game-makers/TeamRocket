70128c8d3e03d7ff0de0d541bae24a4e44265a42
GcNoteX <gcha0054@student.monash.edu>
2025-08-04 20:31:31 +1000

    Simple Square POI Implemented

--- scenes/environments/POI/placeable_poi.gd
+++ scenes/environments/POI/placeable_poi.gd
@@ -2,11 +2,11 @@ extends Node2D
 class_name PlaceablePOI
 
 enum EnterDirectionEnum {
-    NONE,
-    TOP,
-    DOWN,
-    LEFT,
-    RIGHT
+	NONE,
+	TOP,
+	DOWN,
+	LEFT,
+	RIGHT
 }
 
 @export var potential_enter_direction: EnterDirectionEnum
@@ -19,22 +19,22 @@ enum EnterDirectionEnum {
 var map_room: ProceduralRoom # Set in procedural_room.gd
 
 func generate_loot_for_container():
-    var combined_loot_table = Globals.interactable_handler.global_loot_table
-    combined_loot_table.append_array(poi_loot_table)
-    if map_room:
-        combined_loot_table.append_array(map_room.map_loot_table)
+	var combined_loot_table = Globals.interactable_handler.global_loot_table
+	combined_loot_table.append_array(poi_loot_table)
+	if map_room:
+		combined_loot_table.append_array(map_room.map_loot_table)
 
-    for elem in interactable_holder.get_children():
-        if elem is Storage:
-            var crate = elem as Storage
-            if crate.randomized_loot:
-                crate.generate_loot(combined_loot_table)
+	for elem in interactable_holder.get_children():
+		if elem is Storage:
+			var crate = elem as Storage
+			if crate.randomized_loot:
+				crate.generate_loot(combined_loot_table)
 
 func get_enemy_spawns():
-    return spawn_holder.get_children()
+	return spawn_holder.get_children()
 
 func get_enemies():
-    return enemy_holder.get_children()
+	return enemy_holder.get_children()
 
 func get_interactables():
-    return interactable_holder.get_children()
+	return interactable_holder.get_children()
