1aa9740dc5458841af7289fc955741245c31f79a
NKLam <khoalamvn@gmail.com>
2025-08-15 22:56:08 +1000

    Add sfx for rat monarch and fix debug spawn it

--- scripts/ui/Debug/enemy_debug_ui.gd
+++ scripts/ui/Debug/enemy_debug_ui.gd
@@ -7,19 +7,19 @@ func _on_deer_button_pressed() -> void:
 
 func _on_chicken_button_pressed() -> void:
 	spawn_enemy("chicken")
-	
+
 func _on_spider_button_pressed() -> void:
 	spawn_enemy("spider")
-	
+
 func _on_bird_button_pressed() -> void:
 	spawn_enemy("raven")
-	
+
 func _on_stalker_button_pressed() -> void:
 	spawn_enemy("stalker")
-	
+
 func _on_monarch_button_pressed() -> void:
-	spawn_enemy("monarch")
-	
+	spawn_enemy("rat_monarch")
+
 func spawn_enemy(enemy_name):
 	var spawn_position = Globals.player.global_position + Globals.player.direction * 20.0
 	var enemy_spawn: Enemy = summons[enemy_name].instantiate()
