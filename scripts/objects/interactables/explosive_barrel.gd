95317e9b36106403f124541c60f6c947c0ac5429
NKLam <khoalamvn@gmail.com>
2025-08-24 01:07:18 +1000

    Improve explosive barrel explode effect

--- scripts/objects/interactables/explosive_barrel.gd
+++ scripts/objects/interactables/explosive_barrel.gd
@@ -3,10 +3,23 @@ extends StaticBody2D
 @export var fire_scene: PackedScene
 @export var explosion_damage: int = 100
 
+@onready var sprite: Sprite2D = $Sprite2D
 @onready var explosion_range: Area2D = $ExplosionRange
+@onready var explosion_light: PointLight2D = $ExplosionLight
 @onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
+const LIGHT_FADE_SPEED = 10
+
 var exploded = false
+
+func _ready() -> void:
+	explosion_light.visible = false
+
+func _process(delta: float) -> void:
+	if exploded:
+		if explosion_light.energy > 0:
+			explosion_light.energy -= delta * LIGHT_FADE_SPEED
+
 
 func damage(_value: int, _damage_source_position: Vector2 = Vector2.ZERO):
 	if exploded:
@@ -17,8 +30,8 @@ func damage(_value: int, _damage_source_position: Vector2 = Vector2.ZERO):
 	fire_inst.global_position = global_position
 	audio_player.play()
 	# TODO: Play explosive VFX here
-	call_deferred("explosion")
-	visible = false
+	explosion()
+	sprite.visible = false
 	for child in get_children():
 		if child is Area2D:
 			child.process_mode = Node.PROCESS_MODE_DISABLED
@@ -30,6 +43,7 @@ func _on_environment_hitbox_damaged(value: int, damage_source_position: Vector2)
 
 
 func explosion():
+	explosion_light.visible = true
 	for area in explosion_range.get_overlapping_areas():
 		if area.has_method("damage"):
 			area.damage(explosion_damage)
